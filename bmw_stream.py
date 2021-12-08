809409d7b4c2654cd19bd39aa33bf97c72e7f4c9
RoniKonrad <95797928+RoniKonrad@users.noreply.github.com>
2021-12-08 16:09:21 -0700

    Update bmw_stream.py

--- bmw_stream.py
+++ bmw_stream.py
@@ -12,8 +12,8 @@ from pathlib import Path
 #file1 = (base_path / "Ready_data.csv").resolve()
 #file2 = (base_path / "Aggregated Data.csv").resolve()
 
-file1 = "/app/saitproj/main/Ready_data.csv"
-file2 = "/app/saitproj/main/Aggregated Data.csv"
+file1 = "/Ready_data.csv"
+file2 = "/Aggregated Data.csv"
 
 df_original = pd.read_csv(file1)
 df = pd.read_csv(file2,index_col="CUST_KEY")
